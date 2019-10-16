88165e886575a0d200eb9960ddc6868fe1bdc966
Jannis Gumz <jannisgumz@gmail.com>
2019-10-16 21:52:01 +0200

    Remove Action bar at the top of the screen

--- app/src/main/java/com/example/frust/GameActivity.java
+++ app/src/main/java/com/example/frust/GameActivity.java
@@ -11,10 +11,11 @@ public class GameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         setContentView(new GameView(this));
+        getSupportActionBar().hide();
     }
 }
