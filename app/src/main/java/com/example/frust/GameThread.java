45311ec27fc93d56f00b3d55b5bcd282da15deb2
Jannis Gumz <jannisgumz@gmail.com>
2019-10-16 22:24:09 +0200

    Add documentation for the GameThread class

--- app/src/main/java/com/example/frust/GameThread.java
+++ app/src/main/java/com/example/frust/GameThread.java
@@ -3,6 +3,11 @@ package com.example.frust;
 import android.graphics.Canvas;
 import android.view.SurfaceHolder;
 
+/**
+ * @author Jannis Gumz
+ * Manages the game loop
+ */
+
 public class GameThread extends Thread {
     private static int MAX_FPS = 30;
     private double avgFPS;
@@ -11,12 +16,23 @@ public class GameThread extends Thread {
     private boolean running;
     private static Canvas canvas;
 
+    /**
+     * Creates a thread for the currently active GameView. Every tic (at most 30 tics/second)
+     * the thread will update the GameView and refresh the screen.
+     * @param surfaceHolder the given SurfaceHolder
+     * @param gameView the GameView that is to be bound to the thread
+     */
     public GameThread(SurfaceHolder surfaceHolder, GameView gameView) {
         super();
         this.surfaceHolder = surfaceHolder;
         this.gameView = gameView;
     }
 
+    /**
+     * Updates the GameView and draws it to the screen every tick. Tries to achieve a maximum of 30
+     * ticks per second, resulting in 30 frames per second.
+     * Logs the average frames per second to the console.
+     */
     @Override
     public void run() {
         long startTime;
@@ -71,10 +87,18 @@ public class GameThread extends Thread {
 
     }
 
+    /**
+     * Sets the 'running' boolean for the game loop
+     * @param isRunning the value the 'running' variable will be set to
+     */
     public void setRunning(boolean isRunning) {
         running = isRunning;
     }
 
+    /**
+     * Returns the current state of the game loop, whether it is running or not
+     * @return true if the game loop is currently running, else false
+     */
     public boolean isRunning() {
         return running;
     }
